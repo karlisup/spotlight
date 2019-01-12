ef951a85bb06774f735325ff16e47e062c330760
karlis upitis <karlis@upitis.com>
2019-01-13 00:29:30 +0200

    Update gulp to v4 and accordingly update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,17 @@ var gulp = require('gulp'),
 	sass = require('gulp-sass'),
 	browserSync = require('browser-sync').create();
 	
-gulp.task('css', function () {
+gulp.task('css', function() {
 	return gulp.src('scss/*.scss')
 		.pipe(sass({
 	            // outputStyle: 'compressed'
         	}).on('error', sass.logError)
 		)
-		// return into css folder
 		.pipe(gulp.dest('./public/assets/css/'))
 		.pipe(browserSync.stream())
 });
 
-// Static server
-gulp.task('browserSync', function () {
+gulp.task('browserSync', function(){
 	browserSync.init({
 		server: {
 			baseDir: "./public/"
@@ -22,8 +20,8 @@ gulp.task('browserSync', function () {
 	});
 });
 
-gulp.task('watch', function() {
-	gulp.watch('scss/**/*.scss', ['css']);
+gulp.task('watch', function(){
+    gulp.watch('scss/**/*.scss', gulp.series('css'));
 });
 
-gulp.task('default', ['css', 'watch', 'browserSync']);
+gulp.task('default', gulp.series('watch', 'browserSync'));
